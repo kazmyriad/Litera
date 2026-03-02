57aea36cc6822321698b677f8e2584dfe86c7130
SkylerKoba88 <sfk5879@psu.edu>
2026-03-01 22:30:50 -0500

    brain blast

--- testharness/testimagekit.test.js
+++ testharness/testimagekit.test.js
@@ -25,7 +25,7 @@ jest.mock("@imagekit/nodejs", () => {
     },
   }));
   return {
-    _esModule: true,
+    __esModule: true,
     default: ImageKitMock,
     toFile: jest.fn(async () => "mock-uploadable"),
   };
@@ -42,7 +42,7 @@ process.env.VITE_IMAGEKIT_PRIVATE_KEY = "fake-private";
 process.env.VITE_IMAGEKIT_PUBLIC_KEY = "fake-public";
 process.env.VITE_IMAGEKIT_URL_ENDPOINT = "https://fake.endpoint";
 
-const { getAuthParams, uploadBuffer, buildImageUrl, uploadFilePath } = import ("../imagekit");
+const { getAuthParams, uploadBuffer, buildImageUrl, uploadFilePath } = require("../imagekit");
 
 // test getAuthParams
 describe("getAuthParams", () => {
