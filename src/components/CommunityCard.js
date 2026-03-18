21aedac82062dbfc18db83ad16e14a0e3cbc5475
Alyssa F <aiepillios1101@gmail.com>
2026-03-18 00:30:18 -0400

    community things

--- src/components/CommunityCard.js
+++ src/components/CommunityCard.js
@@ -8,6 +8,26 @@ class CommunityCard extends LitElement {
     static get properties(){
         return{
             name: {type:String},
+            thumbnail: {type:String},
+            description: {type:String}
         }
+    }
+
+    constructor(){
+        super();
+        this.name="A Community";
+        this.thumbnail=""; // add placeholder img
+        this.description="A description of the community.";
+    }
+
+    static get styles(){
+        return css`
+
+        `;
+    }
+
+    render(){
+        return html`
+        `;
     }
 }
