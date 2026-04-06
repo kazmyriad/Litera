698499790f045cc7d8ea91150296e5c8e0768383
Alyssa F <aiepillios1101@gmail.com>
2026-04-06 00:04:32 -0400

    Arg

--- src/App.tsx
+++ src/App.tsx
@@ -77,6 +77,8 @@ export class App extends LitElement {
                 return HomePage({currentPath: this.currentPath});
         }
     };
+
+    // ^^ This could be the navbar component? - Alyssa
     
     render(): TemplateResult {
         return html`
